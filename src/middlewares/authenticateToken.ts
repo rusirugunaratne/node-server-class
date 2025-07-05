47a88a6daef5d6662ed68946b31b127d190731a2
rusirugunaratne <info.rusiru@gmail.com>
2025-07-05 10:52:49 +0530

    Token expiry status changed

--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -9,16 +9,19 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
 
     console.log("token", token)
 
+    // 401 -> unauthorized
+    // 403 -> forbidden
+
     if (!token) {
-      throw new APIError(401, "Access token not found")
+      throw new APIError(403, "Access token not found")
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err, decoded) => {
       if (err) {
         if (err instanceof TokenExpiredError) {
-          throw new APIError(401, "Access token expired")
+          throw new APIError(403, "Access token expired")
         } else if (err instanceof JsonWebTokenError) {
-          throw new APIError(401, "Invalid access token")
+          throw new APIError(403, "Invalid access token")
         } else {
           throw new APIError(500, "Access token error")
         }
